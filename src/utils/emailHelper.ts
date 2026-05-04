2fe9c2d57e273c514fd92df628c4c8a55a2d84bf
ahmed harraz <166037713+harraz63@users.noreply.github.com>
2026-05-04 14:59:19 +0300

    Refactor emailHelper to use TypeScript interfaces for sendEmail parameters

--- src/utils/emailHelper.ts
+++ src/utils/emailHelper.ts
@@ -1,12 +1,19 @@
 import nodemailer from 'nodemailer';
 import { EventEmitter } from 'node:events';
 
+interface SendEmailParams {
+  to: string;
+  subject: string;
+  content: string;
+  attachments?: any[];
+}
+
 export const sendEmail = async ({
   to,
   subject,
   content,
   attachments = [],
-}) => {
+}: SendEmailParams) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -28,6 +35,6 @@ export const sendEmail = async ({
 
 export const emitter = new EventEmitter();
 
-emitter.on('sendEmail', (args: any) => {
+emitter.on('sendEmail', (args: SendEmailParams) => {
   sendEmail(args);
 });
